a3ee4fcd53b44dda530e129ac06ea62b6ad9de05
NarendraSinghKhinchi <110231704+NarendraSinghKhinchi@users.noreply.github.com>
2022-11-21 16:01:09 +0530

    Time: 5 ms (89.84%), Space: 46.6 MB (95.15%) - LeetHub

---
+++ 0386-lexicographical-numbers/0386-lexicographical-numbers.java
@@ -0,0 +1,18 @@
+class Solution {
+    public List<Integer> lexicalOrder(int n) {
+        
+        List<Integer> list = new ArrayList<>();
+        for(int i = 1 ; i <= 9 ; i++){
+            recurs(list ,i , n);
+        }
+       return list ;
+    }
+    public void recurs(List<Integer> list ,int i, int n){
+        if(i > n)return ;
+        list.add(i);
+        i = i * 10 ;
+        for(int j = 0 ; j <= 9 ; j++){
+            recurs(list , i + j , n );
+        }
+    }
+}
