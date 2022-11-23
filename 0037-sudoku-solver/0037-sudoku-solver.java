4c7f2c50d11c6fc4d05e049b1767def473d95ec1
NarendraSinghKhinchi <110231704+NarendraSinghKhinchi@users.noreply.github.com>
2022-11-23 15:39:53 +0530

    Time: 11 ms (74.93%), Space: 42.3 MB (16.25%) - LeetHub

---
+++ 0037-sudoku-solver/0037-sudoku-solver.java
@@ -0,0 +1,56 @@
+class Solution {
+    public void solveSudoku(char[][] board) {
+        sudoku(board , 0 , 0);
+    }
+    public boolean sudoku(char[][]board, int row , int col){
+        if(row == 9){
+            // it means we found our solution
+            return true ;
+        }
+        int nrow = row ;
+        int ncol = col ;
+        if(col == 8){
+            nrow = row + 1 ; 
+            ncol = 0 ;
+        }else{
+            ncol = col + 1 ;
+        }
+        if(board[row][col] != '.'){
+            boolean bool = sudoku(board , nrow , ncol);
+            if(bool)return true;
+        }else{
+            for(int i = 1 ; i <= 9 ; i++){
+                char ch = (char)(i + '0') ;
+                if(safe(board , row , col , ch)){
+                    board[row][col] = ch ;
+                    boolean bool = sudoku(board , nrow , ncol);
+                    if(bool)return true ;
+                    board[row][col] = '.' ;
+                }
+            }
+        }
+        return false ;
+    }
+    public boolean safe(char[][] board , int row , int col , char ch){
+        
+        // check horizontally 
+        for(int i = 0 ; i < 9 ; i++){
+            if(board[row][i] == ch )return false ;
+        }
+        // check vertically 
+        for(int j = 0 ; j < 9 ; j++){
+            if(board[j][col] == ch )return false ;
+        }
+        // check in the current cube
+        // int crow = row % 3 ;
+        // int ccol = col % 3 ;
+        row = row / 3 * 3 ; 
+        col = col / 3 * 3  ;
+        for(int i = 0 ; i < 3 ; i++){
+            for(int j = 0 ; j < 3 ; j++){
+                if(board[row + i][col + j] == ch)return false; 
+            }
+        }
+        return true ;
+    }
+}
