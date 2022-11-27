a6c35bb4fd48d394d513934956a7eca5f77216f8
NarendraSinghKhinchi <110231704+NarendraSinghKhinchi@users.noreply.github.com>
2022-11-27 10:20:50 +0530

    Time: 7 ms (86.94%), Space: 54.7 MB (87.65%) - LeetHub

---
+++ 0221-maximal-square/0221-maximal-square.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int maximalSquare(char[][] matrix) {
+        int dp[][] = new int[matrix.length][matrix[0].length];
+        int max = 0 ;
+        for(int i = dp.length - 1 ; i >= 0 ; i--){
+            for(int j = dp[0].length - 1 ; j >= 0 ; j--){
+                if(matrix[i][j] == '0'){
+                    dp[i][j] = 0 ;
+                    continue ;
+                }
+                if(i == dp.length - 1){
+                    dp[i][j] = matrix[i][j] - '0' ;
+                }else if(j == dp[0].length -1){
+                    dp[i][j] = matrix[i][j] - '0' ;
+                }else{
+                    dp[i][j] = Math.min(dp[i+1][j] , Math.min(dp[i][j+1] , dp[i+1][j+1])) + 1;
+                }
+                max = Math.max(max , dp[i][j]);
+                
+            }
+            
+        }
+        return max*max ;
+    }
+}
