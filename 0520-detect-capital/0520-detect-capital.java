655fcccf3233a9736b8781b40bfd33cf356d9c25
NarendraSinghKhinchi <110231704+NarendraSinghKhinchi@users.noreply.github.com>
2023-01-02 19:55:55 +0530

    Time: 1 ms (100.00%), Space: 40.4 MB (95.10%) - LeetHub

--- 0520-detect-capital/0520-detect-capital.java
+++ 0520-detect-capital/0520-detect-capital.java
@@ -1,20 +1,20 @@
 class Solution {
     public boolean detectCapitalUse(String word) {
-        char frst = word.charAt(0) ;
-        boolean cap = false ;
         boolean small = false ;
-        if(frst <= 'z' && frst >= 'a'){
+        boolean cap = false ;
+        if(word.charAt(0) <= 'z' && word.charAt(0) >= 'a'){
             small = true ;
         }
         for(int i = 1 ; i < word.length() ; i++){
             char ch = word.charAt(i) ;
-            if(ch >= 'A' && ch <= 'Z'){
-                if(small)return false ;
-                cap = true ;
-            }else{
+            if(ch <= 'z' && ch >= 'a'){
                 if(cap)return false ;
                 small = true ;
+            }else{
+                if(small)return false ;
+                cap = true ;
             }
+            
         }
         return true ;
     }
