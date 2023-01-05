79c391cb638f532c20dce39d00ccf7425981a92d
NarendraSinghKhinchi <110231704+NarendraSinghKhinchi@users.noreply.github.com>
2023-01-05 23:15:45 +0530

    Time: 51 ms (20.30%), Space: 81.9 MB (5.03%) - LeetHub

--- 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.java
+++ 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.java
@@ -1,21 +1,34 @@
 class Solution {
     public int makeConnected(int n, int[][] connections) {
-        if (connections.length < n - 1) return -1; // To connect all nodes need at least n-1 edges
-        List<Integer>[] graph = new List[n];
-        for (int i = 0; i < n; i++) graph[i] = new ArrayList<>();
-        for (int[] c : connections) {
-            graph[c[0]].add(c[1]);
-            graph[c[1]].add(c[0]);
+        if(connections.length < n-1)return -1 ;
+        HashMap<Integer,ArrayList<Integer>> hm = new HashMap<>();
+        for(int i = 0 ; i < n; i++){
+            hm.put(i , new ArrayList<>()) ;
         }
-        int components = 0;
-        boolean[] visited = new boolean[n];
-        for (int v = 0; v < n; v++) components += dfs(v, graph, visited);
-        return components - 1; // Need (components-1) cables to connect components together
+        for(int i = 0 ; i < connections.length ; i++){
+            hm.get(connections[i][0]).add(connections[i][1]) ;
+            hm.get(connections[i][1]).add(connections[i][0]) ;
+        }
+        // System.out.println(hm) ;
+        boolean visited[] = new boolean[n] ;
+        int count = 0 ; 
+        for(int i = 0 ; i < visited.length ; i++){
+            if(!visited[i]){
+                visited[i] = true ;
+                count++ ;
+                dfs(hm, visited , i) ;
+            }
+        }
+        return count-1 ;
     }
-    int dfs(int u, List<Integer>[] graph, boolean[] visited) {
-        if (visited[u]) return 0;
-        visited[u] = true;
-        for (int v : graph[u]) dfs(v, graph, visited);
-        return 1;
+    public void dfs(HashMap<Integer,ArrayList<Integer>> graph, boolean []visited , int v){
+        
+        ArrayList<Integer> list = graph.get(v) ;
+        for(int x : list){
+            if(!visited[x]){
+                visited[x] = true ;
+                dfs(graph , visited , x) ;
+            }
+        }
     }
 }
