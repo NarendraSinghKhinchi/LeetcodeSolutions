cf33eb8bb3b2f90ee44965a38adef7067e62f0e5
NarendraSinghKhinchi <110231704+NarendraSinghKhinchi@users.noreply.github.com>
2023-01-11 10:27:42 +0530

    Time: 68 ms (51.76%), Space: 85.6 MB (61.18%) - LeetHub

--- 1443-minimum-time-to-collect-all-apples-in-a-tree/1443-minimum-time-to-collect-all-apples-in-a-tree.java
+++ 1443-minimum-time-to-collect-all-apples-in-a-tree/1443-minimum-time-to-collect-all-apples-in-a-tree.java
@@ -1,33 +1,30 @@
 class Solution {
-    public int minTime(int n, int[][] edges, List<Boolean> hasApple) {
-        HashMap<Integer,ArrayList<Integer>> tree = new HashMap<>();
-        for(int i = 0 ; i < n ; i++)tree.put(i , new ArrayList<>());
-        for(int ed[] : edges){
-            tree.get(ed[0]).add(ed[1]) ;
-            tree.get(ed[1]).add(ed[0]);
-        }
-        boolean visited[] = new boolean[n];
-        int time = dfs(tree , 0 , hasApple , visited);
-        if(time == -1)return 0 ;
-        return time ;
-    }
-    public int dfs(HashMap<Integer,ArrayList<Integer>> tree , int node , List<Boolean> hasApple , boolean visited[]){
-        if(node > hasApple.size())return -1 ;
+    public int dfs(int node, int parent, Map<Integer, List<Integer>> adj,
+            List<Boolean> hasApple) {
+        if (!adj.containsKey(node))
+            return 0;
 
-        visited[node] = true ;
-        int curr = 0 ;
-        for(int x : tree.get(node)){
-            if(visited[x] == false){
-                int c = dfs(tree , x , hasApple , visited);
-                if(c != -1)curr += c + 2 ;
-            }
+        int totalTime = 0, childTime = 0;
+        for (int child : adj.get(node)) {
+            if (child == parent)
+                continue;
+            childTime = dfs(child, node, adj, hasApple);
+            // childTime > 0 indicates subtree of child has apples. Since the root node of the
+            // subtree does not contribute to the time, even if it has an apple, we have to check it
+            // independently.
+            if (childTime > 0 || hasApple.get(child))
+                totalTime += childTime + 2;
         }
-        
-        if(hasApple.get(node)){
-            return curr ;
-        }
-        if(curr == 0)curr = -1 ;
-        return curr ;
+        return totalTime;
     }
-    
+
+    public int minTime(int n, int[][] edges, List<Boolean> hasApple) {
+        Map<Integer, List<Integer>> adj = new HashMap<>();
+        for (int[] edge : edges) {
+            int a = edge[0], b = edge[1];
+            adj.computeIfAbsent(a, value -> new ArrayList<Integer>()).add(b);
+            adj.computeIfAbsent(b, value -> new ArrayList<Integer>()).add(a);
+        }
+        return dfs(0, -1, adj, hasApple);
+    }
 }
