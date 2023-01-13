ded2e669e0a1694116b9ebcbfb41f140b33e9a78
NarendraSinghKhinchi <110231704+NarendraSinghKhinchi@users.noreply.github.com>
2023-01-13 18:43:48 +0530

    Time: 1753 ms (5.27%), Space: 103.7 MB (75.00%) - LeetHub

---
+++ 2246-longest-path-with-different-adjacent-characters/2246-longest-path-with-different-adjacent-characters.java
@@ -0,0 +1,36 @@
+class Solution {
+    public int longestPath(int[] parent, String s) {
+        ArrayList<Integer> tree[] = new ArrayList[parent.length] ;
+        for(int i = 0 ; i < parent.length; i++)tree[i] = new ArrayList<>();
+        
+        for(int i = 1 ; i < parent.length ; i++){
+            // tree[i].add(parent[i]);
+            tree[parent[i]].add(i) ;
+        }
+        for(ArrayList<Integer> child : tree)System.out.println(child);
+        this.max = 0 ;
+        dfs(tree , s , 0 , -1);
+        return max ;
+    }
+    int max = 0 ;
+    public int dfs(ArrayList<Integer> tree[] ,String s , int v , int parent ){
+        
+        int first = 0 ;
+        int second = 0 ;
+        for(int x : tree[v]){
+            int nxt = dfs(tree  , s , x , parent);
+            if(s.charAt(x) != s.charAt(v)){
+                if(nxt > first){
+                    second = first ;
+                    first = nxt ;
+                }else if(nxt > second){
+                    second = nxt ;
+                }
+            }
+        }
+        
+        max = Math.max(max , first + second + 1);
+        
+        return first+1 ;
+    }
+}
