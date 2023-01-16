df27b1557579b5dce0db314b1defda3317e4d057
NarendraSinghKhinchi <110231704+NarendraSinghKhinchi@users.noreply.github.com>
2023-01-16 16:10:18 +0530

    Time: 11 ms (86.92%), Space: 42.3 MB (87.69%) - LeetHub

--- 1003-check-if-word-is-valid-after-substitutions/1003-check-if-word-is-valid-after-substitutions.java
+++ 1003-check-if-word-is-valid-after-substitutions/1003-check-if-word-is-valid-after-substitutions.java
@@ -1,6 +1,6 @@
 class Solution {
     public boolean isValid(String s) {
-        Stack<Character> st = new Stack<>();
+       Deque<Character> st = new ArrayDeque<>();
         for(int i =0  ; i < s.length() ; i++){
             char ch = s.charAt(i);
             if(ch == 'a'){
