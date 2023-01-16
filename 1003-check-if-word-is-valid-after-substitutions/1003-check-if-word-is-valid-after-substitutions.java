5a0a48aa2bc964c63346463039a325b440e5b4aa
NarendraSinghKhinchi <110231704+NarendraSinghKhinchi@users.noreply.github.com>
2023-01-16 16:07:57 +0530

    Time: 21 ms (61.54%), Space: 42.4 MB (76.15%) - LeetHub

---
+++ 1003-check-if-word-is-valid-after-substitutions/1003-check-if-word-is-valid-after-substitutions.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> st = new Stack<>();
+        for(int i =0  ; i < s.length() ; i++){
+            char ch = s.charAt(i);
+            if(ch == 'a'){
+                st.push(ch);
+            }else if(ch == 'b'){
+                if(st.size() > 0 && st.peek() == 'a')st.pop();
+                else return false ;
+                st.push(ch);
+            }else{
+                if(st.size() > 0 && st.peek() == 'b')st.pop();
+                else st.push(ch);
+            }
+        }
+        if(st.size() != 0)return false ;
+        
+        return true ;
+    }
+}
