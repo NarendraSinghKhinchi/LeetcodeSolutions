b58b981b0515f84b0bdfdfb7d3c0f5d41f4b50fb
NarendraSinghKhinchi <110231704+NarendraSinghKhinchi@users.noreply.github.com>
2023-01-23 15:26:33 +0530

    Time: 15 ms (34.97%), Space: 75 MB (5.79%) - LeetHub

---
+++ 0997-find-the-town-judge/0997-find-the-town-judge.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int findJudge(int n, int[][] trust) {
+        HashMap<Integer,ArrayList<Integer>> hm = new HashMap<>();
+        for(int i = 1 ; i <= n ; i++){
+            hm.put(i , new ArrayList<>());
+        }
+        // in hashmap list we store the person who trust i or key 
+        for(int i = 0 ; i < trust.length ; i++){
+            int person = trust[i][0] ;
+            int trusted = trust[i][1] ;
+            if(hm.containsKey(trusted)){
+                ArrayList<Integer> list = hm.get(trusted);
+                list.add(person);
+            }
+            if(hm.containsKey(person)){
+                hm.remove(person);
+            }
+        }
+        if(hm.size() == 0)return -1 ;
+        if(hm.size() > 1)System.out.println("done done you're done");
+        for(int key : hm.keySet()){
+            ArrayList<Integer> list = hm.get(key);
+            if(list.size() == n-1)return key ;
+        }
+        return -1 ;
+    }
+}
