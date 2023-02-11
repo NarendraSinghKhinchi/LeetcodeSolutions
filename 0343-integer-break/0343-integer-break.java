ef5f4090df0d2181721298d8c76602b294838320
NarendraSinghKhinchi <110231704+NarendraSinghKhinchi@users.noreply.github.com>
2023-02-11 17:31:43 +0530

    Time: 0 ms (100.00%), Space: 39.1 MB (57.82%) - LeetHub

--- 0343-integer-break/0343-integer-break.java
+++ 0343-integer-break/0343-integer-break.java
@@ -1,17 +1,20 @@
 class Solution {
     public int integerBreak(int n) {
         
-        return breaker(n , n-1) ;
+        int dp[] = new int[n+1];
+        Arrays.fill(dp , -1);
+        return breaker(n , n-1 , dp) ;
     }
-    public int breaker(int n , int idx){
+    public int breaker(int n , int idx , int dp[]){
         
         if(n== 0 || idx == 0)return 1 ;
+        if(dp[n] != -1)return dp[n] ;
+        if(idx > n)return breaker(n , idx-1 , dp);
+        int taken = breaker(n-idx , idx , dp) * idx;
+        int nottaken = breaker(n , idx-1 , dp);
         
-        if(idx > n)return breaker(n , idx-1);
-        int taken = breaker(n-idx , idx) * idx;
-        int nottaken = breaker(n , idx-1);
-        
-        return Math.max(taken , nottaken);
+        dp[n] =  Math.max(taken , nottaken);
+        return dp[n] ;
     }
 }
     
