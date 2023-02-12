a250d14332c5aab336c7419a245e1aaed2703d98
NarendraSinghKhinchi <110231704+NarendraSinghKhinchi@users.noreply.github.com>
2023-02-12 12:32:54 +0530

    Time: 55 ms (94.87%), Space: 106.4 MB (48.08%) - LeetHub

---
+++ 2477-minimum-fuel-cost-to-report-to-the-capital/2477-minimum-fuel-cost-to-report-to-the-capital.java
@@ -0,0 +1,31 @@
+class Solution {
+    public long minimumFuelCost(int[][] roads, int seats) {
+        ArrayList<Integer> []graph = new ArrayList[roads.length+1] ;
+        
+        for(int i = 0 ; i<graph.length ;  i++)graph[i] = new ArrayList<>() ;
+        for(int road[] : roads){
+            graph[road[0]].add(road[1]) ;
+            graph[road[1]].add(road[0]) ;
+        }
+        this.fuel = 0l ;
+        dfs(graph , seats , -1 , 0);
+        return fuel ;
+    }
+    long fuel ;
+    public long dfs(ArrayList<Integer> graph [] , int seats , int parent , int node){
+        
+        long rep = 1 ;
+        
+        for(int adj : graph[node]){
+            if(adj != parent){
+                rep += dfs(graph , seats , node , adj);
+            }
+        }
+        
+        if(node != 0){
+            fuel += Math.ceil((double) rep / seats) ;
+        }
+        return rep ;
+    }
+    
+}
