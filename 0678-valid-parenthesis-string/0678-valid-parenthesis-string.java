9804f21065a4aa988b2d22e28d2c2cf13506684d
NarendraSinghKhinchi <110231704+NarendraSinghKhinchi@users.noreply.github.com>
2023-03-17 22:27:45 +0530

    Time: 0 ms (100.00%), Space: 40.3 MB (58.12%) - LeetHub

--- 0678-valid-parenthesis-string/0678-valid-parenthesis-string.java
+++ 0678-valid-parenthesis-string/0678-valid-parenthesis-string.java
@@ -1,34 +1,13 @@
 class Solution {
     public boolean checkValidString(String s) {
-        Stack<Character> st = new Stack<>();
         
-        for(int i = 0 ; i < s.length() ; i++){
-            char ch = s.charAt(i);
-            if(ch == '*' || ch == '(')st.push(ch) ;
-            if(ch == ')'){
-                int star = 0 ;
-                while(st.size() > 0 && st.peek() != '('){
-                    if(st.peek() == '*')star += 1 ;
-                    st.pop();
-                }
-                if(st.size() == 0 && star == 0)return false ;
-                if(st.size() > 0 && st.peek() == '(')st.pop();
-                else star -= 1 ;
-                while(star-- > 0){
-                    st.push('*') ;
-                }
-            }
-        }
-        int star = 0 ;
-        int open = 0 ;
-        while(st.size() > 0){
-            if(st.peek() == '*')star += 1 ;
-            else open += 1 ;
-            
-            if(open > star)return false ;
-            st.pop();
-        }
-        
-        return true ;
+       int lo = 0, hi = 0;
+       for (char c: s.toCharArray()) {
+           lo += c == '(' ? 1 : -1;
+           hi += c != ')' ? 1 : -1;
+           if (hi < 0) break;
+           lo = Math.max(lo, 0);
+       }
+       return lo == 0;
     }
 }
