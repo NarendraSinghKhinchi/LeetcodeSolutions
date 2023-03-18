d20db5c89378b1f21b143d047b1a42f6daa519ea
NarendraSinghKhinchi <110231704+NarendraSinghKhinchi@users.noreply.github.com>
2023-03-18 10:48:07 +0530

    Time: 114 ms (14.14%), Space: 50.9 MB (16.81%) - LeetHub

--- 1472-design-browser-history/1472-design-browser-history.java
+++ 1472-design-browser-history/1472-design-browser-history.java
@@ -1,32 +1,31 @@
 class BrowserHistory {
     
-    Deque<String> his;
-    Deque<String> forw ;
+    Stack<String> q ;
+    Stack<String> st ;
     public BrowserHistory(String homepage) {
-        his = new ArrayDeque<>(); 
-        his.push(homepage);
-        forw = new ArrayDeque<>() ;
+        
+        this.q = new Stack<>();
+        this.st = new Stack<>();
+        st.push(homepage);
     }
     
     public void visit(String url) {
-        forw.clear();
-        his.push(url);
+        q = new Stack<>();
+        st.push(url);
     }
     
     public String back(int steps) {
-        while(steps >= 1 && his.size() > 1){
-            forw.push(his.pop());
-            steps-- ;
+        while(steps-- > 0 && st.size() > 1){
+            q.push(st.pop());
         }
-        return his.peek();
+        return st.peek();
     }
     
     public String forward(int steps) {
-        while(steps >= 1 && forw.isEmpty() == false){
-            his.push(forw.pop());
-            steps-- ;
+        while(steps-- > 0  && q.size() > 0){
+            st.push(q.pop());
         }
-        return his.peek();
+        return st.peek();
     }
 }
 
