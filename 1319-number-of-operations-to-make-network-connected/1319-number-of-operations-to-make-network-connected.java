b863f98285655f7b4c26271d1f905bdef03583f9
NarendraSinghKhinchi <110231704+NarendraSinghKhinchi@users.noreply.github.com>
2023-04-05 13:11:38 +0530

    Time: 3 ms (99.31%), Space: 55.2 MB (98.41%) - LeetHub

--- 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.java
+++ 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.java
@@ -2,14 +2,14 @@ class Solution {
     public int makeConnected(int n, int[][] connections) {
         if(connections.length < n-1)return -1 ;
         int dis[] = new int[n];
-        int rank[] = new int[n];
+       
         for(int i = 0 ; i < n ; i++)dis[i] = i ;
         int count = 0 ;
         for(int con[] : connections){
             int p = find(dis , con[0]);
             int q = find(dis , con[1]) ;
             if(p != q){
-                union(dis , rank , con[0] , con[1]) ;
+                union(dis ,  con[0] , con[1]) ;
                 count += 1 ;
             }   
         }
@@ -19,17 +19,12 @@ class Solution {
         if(dis[no]  == no)return no ;
         return dis[no] = find(dis , dis[no]);
     }
-    public void union(int dis[] ,int rank[] , int x , int y){
+    public void union(int dis[] , int x , int y){
         int p = find(dis , x);
         int q = find(dis , y);
-        // if(rank[p] < rank[q]){
-            dis[p] = q ;
-            rank[q]++ ;
-        // }else{
-        //     dis[q] = p ;
-        //     rank[p]++ ;
-        // }
         
+            dis[p] = q ;
+            
         
     }
 }
