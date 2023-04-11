c9e0c03d7f4134c2a97a9d0fdd136197f604f407
NarendraSinghKhinchi <110231704+NarendraSinghKhinchi@users.noreply.github.com>
2023-04-11 16:17:10 +0530

    Time: 1 ms (96.41%), Space: 42.6 MB (66.62%) - LeetHub

---
+++ 0046-permutations/0046-permutations.java
@@ -0,0 +1,24 @@
+class Solution {
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> list = new ArrayList<>();
+        List<Integer> curr = new ArrayList<>();
+        boolean selected[] = new boolean[nums.length];
+        dfs(list , curr , nums , 0 , selected);
+        return list ;
+    }
+    public void dfs(List<List<Integer>> list ,List<Integer> curr , int[]nums , int idx , boolean selected[]){
+        if(idx == nums.length){
+            list.add(new ArrayList<>(curr));
+            return ;
+        }
+        for(int i = 0 ; i < nums.length ; i++){
+            if(selected[i] == false){
+                curr.add(nums[i]);
+                selected[i] = true ;
+                dfs(list , curr , nums , idx+1 , selected);
+                selected[i] = false ;
+                curr.remove(curr.size()-1);
+            }
+        }
+    }
+}
