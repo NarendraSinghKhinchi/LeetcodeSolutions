451cd7486293a6a42f8008b9fc26a03bb9e8e0c7
NarendraSinghKhinchi <110231704+NarendraSinghKhinchi@users.noreply.github.com>
2023-04-17 06:30:45 +0530

    Time: 1 ms (81.26%), Space: 42.3 MB (87.19%) - LeetHub

--- 1431-kids-with-the-greatest-number-of-candies/1431-kids-with-the-greatest-number-of-candies.java
+++ 1431-kids-with-the-greatest-number-of-candies/1431-kids-with-the-greatest-number-of-candies.java
@@ -1,15 +1,26 @@
 class Solution {
-    public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
+    public List<Boolean> kidsWithCandies(int[] candies, int extraCandies){
         List<Boolean> list = new ArrayList<>();
+        // for(int i = 0 ; i < candies.length ; i++){
+        //     boolean bool = true ;
+        //     for(int j = 0 ; j < candies.length ; j++){
+        //         if(candies[i] + extraCandies < candies[j]){
+        //             bool = false ;
+        //             break ;
+        //         }
+        //     }
+        //     list.add(bool);
+        // }
+        
+        int max = Integer.MIN_VALUE ;
         for(int i = 0 ; i < candies.length ; i++){
-            boolean bool = true ;
-            for(int j = 0 ; j < candies.length ; j++){
-                if(candies[i] + extraCandies < candies[j]){
-                    bool = false ;
-                    break ;
-                }
-            }
-            list.add(bool);
+            max = Math.max(candies[i] , max);
+        }
+        for(int i = 0 ; i < candies.length ; i++){
+            int arr = candies[i] + extraCandies ;
+            if(arr < max){
+                list.add(false);
+            }else list.add(true);
         }
         return list ;
     }
