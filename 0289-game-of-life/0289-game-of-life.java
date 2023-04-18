f110e5b8faed2617882f6073802019210a88d2bc
NarendraSinghKhinchi <110231704+NarendraSinghKhinchi@users.noreply.github.com>
2023-04-18 19:56:10 +0530

    Time: 0 ms (100.00%), Space: 40.5 MB (87.13%) - LeetHub

--- 0289-game-of-life/0289-game-of-life.java
+++ 0289-game-of-life/0289-game-of-life.java
@@ -1,14 +1,15 @@
 class Solution {
     public void gameOfLife(int[][] board) {
         
-        int arr[][] = new int[board.length][board[0].length];
+        
         for(int i = 0 ; i < board.length ; i++){
             for(int j =0 ; j < board[0].length ; j++){
-                int count = livenbrs(board , i , j , true);
-                if(board[i][j] == 0 && count == 3){
-                    arr[i][j] = 1 ;
-                }else if(board[i][j] == 1 && (count ==  3 || count == 2)){
-                    arr[i][j] = 1 ;
+                int count = livenbrs(board , i , j , true) + 9 ;
+                
+                if(board[i][j] == 0){
+                    board[i][j] = -count ;
+                }else {
+                    board[i][j] = count ;
                 }
                 
             }
@@ -16,14 +17,29 @@ class Solution {
         
         for(int i = 0 ; i < board.length ; i++){
             for(int j = 0 ; j < board[0].length ; j++){
-                board[i][j] = arr[i][j] ;
+                if(board[i][j] > 0){
+                    int count = board[i][j] % 9 ;
+                    if(count == 2 || count == 3){
+                        board[i][j] = 1 ;
+                    }else board[i][j] = 0 ;
+                }else {
+                    int count = (-board[i][j]) % 9 ;
+                    if(count == 3)
+                        board[i][j] = 1 ;
+                    else board[i][j] = 0 ;
+                }
             }
         }
     }
     public int livenbrs(int [][]board , int i , int j , boolean bool ){
         
         if(i < 0 || j < 0 || i >= board.length || j >= board[0].length )return 0 ;
-        if(!bool)return board[i][j] ;
+        if(!bool){
+            if(board[i][j] == 0 || board[i][j] == 1)return board[i][j] ;
+            
+            if(board[i][j] > 0)return 1 ;
+            return 0 ;
+        }
         int count = 0 ;
         
         count += livenbrs(board , i-1 , j , false)  ;
