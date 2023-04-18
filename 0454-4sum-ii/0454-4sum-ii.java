566b2f00da4be1a9a724a6914a3c16307c6eb798
NarendraSinghKhinchi <110231704+NarendraSinghKhinchi@users.noreply.github.com>
2023-04-18 16:09:29 +0530

    Time: 140 ms (54.48%), Space: 42.6 MB (72.54%) - LeetHub

--- 0454-4sum-ii/0454-4sum-ii.java
+++ 0454-4sum-ii/0454-4sum-ii.java
@@ -1,27 +1,28 @@
 class Solution {
     public int fourSumCount(int[] nums1, int[] nums2, int[] nums3, int[] nums4) {
         
-        // HashMap<Integer,Integer> hm = help(nums1 , nums2);
-        // HashMap<Integer,Integer> hm1 = help(nums3 , nums4);
+        // Map<Integer,Integer> hm = help(nums1 , nums2);
+        // Map<Integer,Integer> hm1 = help(nums3 , nums4);
         // int count = 0 ;
         // for(int x : hm.keySet()){
         //     int y = -x ;
         //     if(hm1.containsKey(y)){
-        //         count += Math.max(hm.get(x) , hm1.get(y));
+        //         count += hm.get(x) + hm1.get(y);
         //     }
         // }
         // return count ;
-        Map<Integer, Integer> map = new HashMap<>();
+        
         int [] A = nums1 ;
         int B[] = nums2 ;
         int C[] = nums3 ;
         int D[] = nums4 ;
-        for(int i=0; i<C.length; i++) {
-            for(int j=0; j<D.length; j++) {
-                int sum = C[i] + D[j];
-                map.put(sum, map.getOrDefault(sum, 0) + 1);
-            }
-        }
+        Map<Integer, Integer> map = help(C , D);
+        // for(int i=0; i<C.length; i++) {
+        //     for(int j=0; j<D.length; j++) {
+        //         int sum = C[i] + D[j];
+        //         map.put(sum, map.getOrDefault(sum, 0) + 1);
+        //     }
+        // }
 
         int res=0;
         for(int i=0; i<A.length; i++) {
@@ -32,8 +33,8 @@ class Solution {
 
         return res;
     }
-    public HashMap<Integer,Integer> help(int []arr , int []num){
-        HashMap<Integer,Integer> hm = new HashMap<>();
+    public Map<Integer,Integer> help(int []arr , int []num){
+        Map<Integer,Integer> hm = new HashMap<>();
         for(int i = 0 ; i < arr.length ; i++){
             for(int j = 0 ; j < num.length ; j++){
                 int sum = arr[i] + num[j] ;
