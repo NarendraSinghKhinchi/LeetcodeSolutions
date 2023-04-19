d1b3905971640a49b5c6843ab8eaebfbd1446f87
NarendraSinghKhinchi <110231704+NarendraSinghKhinchi@users.noreply.github.com>
2023-04-19 08:21:23 +0530

    Time: 6 ms (99.70%), Space: 52.9 MB (94.35%) - LeetHub

--- 1372-longest-zigzag-path-in-a-binary-tree/1372-longest-zigzag-path-in-a-binary-tree.java
+++ 1372-longest-zigzag-path-in-a-binary-tree/1372-longest-zigzag-path-in-a-binary-tree.java
@@ -17,25 +17,22 @@ class Solution {
     public int longestZigZag(TreeNode root) {
       
         max = 0 ;
-        longest(root);
+        longest(root , -1 , 0);
         return max ;
     }
     int max = 0 ;
-    private Node longest(TreeNode root){
-        if(root == null)return new Node(0,0) ;
-        if(root.left == null && root.right == null)return new Node(1,1) ;
-        
-        Node left = longest(root.left);
-        Node right = longest(root.right) ;
-        int curr = Math.max(left.right , right.left) ;
-        max = Math.max(max , curr) ;
-        return new Node(left.right+1 , right.left+1) ;
-    }
-    private class Node{
-        int left , right ;
-        Node(int l , int r){
-            this.left = l ;
-            this.right = r ;
+    private void longest(TreeNode root , int dir , int len){
+        if(root == null){
+            return ;
+        }
+        max = Math.max(max , len);
+        // 0 means left now you should go right // 1 means left
+        if(dir == 0){
+            longest(root.right , 1 , len+1);
+            longest(root.left , 0 , 1);
+        }else{
+            longest(root.left , 0 , len+1);
+            longest(root.right , 1 , 1);
         }
     }
 }
