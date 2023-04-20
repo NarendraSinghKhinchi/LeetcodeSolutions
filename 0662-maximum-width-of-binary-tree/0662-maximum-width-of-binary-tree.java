b7579df84b9f87b93ea110f2dfa0220960920171
NarendraSinghKhinchi <110231704+NarendraSinghKhinchi@users.noreply.github.com>
2023-04-20 08:12:12 +0530

    Time: 1 ms (100.00%), Space: 41.8 MB (96.32%) - LeetHub

--- 0662-maximum-width-of-binary-tree/0662-maximum-width-of-binary-tree.java
+++ 0662-maximum-width-of-binary-tree/0662-maximum-width-of-binary-tree.java
@@ -15,41 +15,23 @@
  */
 class Solution {
     public int widthOfBinaryTree(TreeNode root) {
-        int max_width = 1 ;
-        Queue<Pair> q = new ArrayDeque<>();
-        q.add(new Pair(root , 0)) ;
-        
-        while(q.size() > 0){
-            int size = q.size() ;
-            int first = 0 ;
-            int last = 0 ;
-            for(int i = 1 ; i <= size  ; i++){
-                Pair rem = q.remove() ;
-                if(i == 1){
-                   first = rem.idx ;
-                   last = rem.idx ;
-                }
-                if(i == size){
-                    last = rem.idx;
-                }
-                 
-                if(rem.node.left != null)q.add(new Pair(rem.node.left , rem.idx*2+1)) ;
-                if(rem.node.right != null)q.add(new Pair(rem.node.right , rem.idx*2+2)) ;
-            }
-            
-            int curr_width = last - first + 1 ;
-            max_width = Math.max(max_width , curr_width);
-
-            
-        }
-        return max_width ;
+        this.list = new ArrayList<>();
+        this.max = 1 ;
+        travel(root , 0 , 1);
+        return max ;
     }
-    class Pair{
-        int idx ;
-        TreeNode node ;
-        Pair(TreeNode node , int idx){
-            this.node = node ;
-            this.idx = idx ;
+    ArrayList<Integer> list ;
+    int max ;
+    public void travel(TreeNode node , int idx , int level){
+        if(node == null)return ;
+        if(list.size() < level){
+            list.add(idx);
+        }else{
+            int width = idx - list.get(level-1) + 1 ;
+            max = Math.max(width , max);
         }
+        travel(node.left , 2*idx+1 , level+1);
+        travel(node.right , 2*idx+2 , level+1);
+        
     }
 }
