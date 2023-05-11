36be1530e0c57d26140ea0d9bff3c3050187d17b
NarendraSinghKhinchi <110231704+NarendraSinghKhinchi@users.noreply.github.com>
2023-05-11 10:45:54 +0530

    Time: 5 ms (92.56%), Space: 41.9 MB (90.82%) - LeetHub

--- 1035-uncrossed-lines/1035-uncrossed-lines.java
+++ 1035-uncrossed-lines/1035-uncrossed-lines.java
@@ -1,10 +1,24 @@
 class Solution {
     public int maxUncrossedLines(int[] nums1, int[] nums2) {
         
-        int dp[][] = new int[nums1.length][nums2.length];
-        for(int d[] : dp)Arrays.fill(d ,-1);
-        int ans = max(nums1 ,nums2 , 0 , 0 , dp);
-        return ans ;
+        int dp[][] = new int[nums1.length+1][nums2.length+1];
+        int ans = 0 ;
+        for(int i = nums1.length-1 ; i >= 0 ; i--){
+            for(int j = nums2.length-1 ; j >= 0 ; j--){
+                
+                if(nums1[i] == nums2[j]){
+                    dp[i][j] = 1 + dp[i+1][j+1] ;
+                }else{
+                    dp[i][j] = Math.max(dp[i+1][j] , dp[i][j+1]) ;
+                }
+                
+            }
+        }
+        
+        // for(int d[] : dp)Arrays.fill(d ,-1);
+        // int ans = max(nums1 ,nums2 , 0 , 0 , dp);
+        
+        return dp[0][0] ;
     }
     public int max(int nums[] , int arr[] , int i , int j , int dp[][]){
         if(i == nums.length || j == arr.length )return 0 ;
