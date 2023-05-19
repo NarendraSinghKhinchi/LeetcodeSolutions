9397cc56246d57b93d319e7fb6e6f7b543e34f8c
NarendraSinghKhinchi <110231704+NarendraSinghKhinchi@users.noreply.github.com>
2023-05-19 20:54:30 +0530

    Time: 1 ms (66.41%), Space: 44 MB (6.63%) - LeetHub

--- 0785-is-graph-bipartite/0785-is-graph-bipartite.java
+++ 0785-is-graph-bipartite/0785-is-graph-bipartite.java
@@ -1,32 +1,25 @@
 class Solution {
     public boolean isBipartite(int[][] graph) {
-        Queue<Pair> q = new ArrayDeque<>();
-        int visited[] = new int[graph.length] ;
-        Arrays.fill(visited,-1);
-        for(int v = 0 ; v < graph.length ; v++){
-            if(visited[v] == -1)
-                q.add(new Pair(v,0)) ;
-            while(q.size() > 0){
-                Pair rem = q.remove();
-                if(visited[rem.v] != -1){
-                    if(rem.dis != visited[rem.v] )return false ;
-                    // else continue ;
-                }
-                visited[rem.v] = rem.dis ;
-                for(int j = 0 ; j < graph[rem.v].length ; j++){
-                    if(visited[graph[rem.v][j]] == -1){
-                        q.add(new Pair(graph[rem.v][j] , rem.dis+1));
+        int visited[] = new int[graph.length];
+        Arrays.fill(visited , -1);
+        Queue<int[]> q = new ArrayDeque<>();
+        for(int i = 0 ; i < graph.length ; i++){
+            if(visited[i] == -1){
+                q.add(new int[]{i , 0});
+                visited[i] = 0 ;
+                while(!q.isEmpty()){
+                    int rem[] = q.remove();
+                    for(int nbr : graph[rem[0]]){
+                        if(visited[nbr] == -1){
+                            visited[nbr] = rem[1] == 0 ? 1 : 0 ;
+                            q.add(new int[]{nbr , visited[nbr]});
+                        }else if(visited[nbr] == rem[1])return false ;
                     }
                 }
             }
         }
+        
+        
         return true ;
-    }
-    private class Pair{
-        int v , dis ;
-        Pair(int v , int dis){
-            this.v = v ;
-            this.dis = dis ;
-        }
     }
 }
