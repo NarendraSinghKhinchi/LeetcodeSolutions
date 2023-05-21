176a42f96564543463227b8875c304b40aa843ed
NarendraSinghKhinchi <110231704+NarendraSinghKhinchi@users.noreply.github.com>
2023-05-21 07:28:02 +0530

    Time: 8 ms (86.74%), Space: 44.8 MB (5.97%) - LeetHub

--- 0934-shortest-bridge/0934-shortest-bridge.java
+++ 0934-shortest-bridge/0934-shortest-bridge.java
@@ -1,12 +1,11 @@
 class Solution {
     public int shortestBridge(int[][] grid) {
         this.q = new ArrayDeque<>();
-        boolean visited[][] = new boolean[grid.length][grid[0].length];
         for(int i = 0 ; i < grid.length ; i++){
             boolean flag = false ;
             for(int j = 0 ; j < grid[0].length ; j++){
                 if(grid[i][j] == 1){
-                    dfs(grid , i , j , visited);
+                    dfs(grid , i , j );
                     flag = true ;
                     break ;
                 }
@@ -24,9 +23,9 @@ class Solution {
                 for(int dir[] : dirs){
                     int nr = rem[0]+dir[0] ;
                     int nc = rem[1]+dir[1] ;
-                    if(nr < 0 || nc < 0 || nr == grid.length || nc == grid[0].length || visited[nr][nc] )continue ;
+                    if(nr < 0 || nc < 0 || nr == grid.length || nc == grid[0].length || grid[nr][nc] == 2 )continue ;
                     if(grid[nr][nc] == 1)return count-1 ;
-                    visited[nr][nc] = true ;
+                    grid[nr][nc] = 2 ;
                     q.add(new int[]{nr , nc});
                 }
             }
@@ -35,16 +34,16 @@ class Solution {
         return -1;
     }
     Queue<int[]> q ;
-    public void dfs(int [][] grid , int i , int j , boolean visited[][]){
-        if(i < 0 || j < 0 || i >= grid.length || j >= grid[0].length  || visited[i][j] || grid[i][j] != 1)return ;
+    public void dfs(int [][] grid , int i , int j ){
+        if(i < 0 || j < 0 || i >= grid.length || j >= grid[0].length  || grid[i][j] != 1)return ;
         
-        visited[i][j] = true ;
+        
         grid[i][j] = 2 ;
         q.add(new int[]{i , j});
         
-        dfs(grid , i+1 , j , visited);
-        dfs(grid , i-1 , j , visited);
-        dfs(grid , i , j+1 , visited);
-        dfs(grid, i , j-1 , visited);
+        dfs(grid , i+1 , j );
+        dfs(grid , i-1 , j );
+        dfs(grid , i , j+1 );
+        dfs(grid, i , j-1 );
     }
 }
