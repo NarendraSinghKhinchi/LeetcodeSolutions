5421106f06716f7ab4c8e4e1d359231f5c7ee75f
NarendraSinghKhinchi <110231704+NarendraSinghKhinchi@users.noreply.github.com>
2023-05-26 10:54:36 +0530

    Time: 69 ms (20.11%), Space: 44.4 MB (12.79%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.java
+++ 0045-jump-game-ii/0045-jump-game-ii.java
@@ -1,16 +1,15 @@
 class Solution {
     public int jump(int[] nums) {
-        int dp[] = new int[nums.length] ;
-        Arrays.fill(dp , -1);
-        dp[nums.length - 1] = 0 ;
-        for(int i = nums.length - 2 ; i >= 0 ; i--){
+        int dp[] = new int[nums.length];
+        Arrays.fill(dp , Integer.MAX_VALUE);
+        dp[nums.length-1] = 0 ;
+        
+        for(int i = nums.length-2 ;i >= 0 ; i--){
             int jump = nums[i] ;
-            for(int j = 1 ; j <= jump && i+j < nums.length ; j++){
-                if(dp[i] == -1 && dp[i+j] != -1){
-                    dp[i] = dp[i+j]+1 ;
-                }else if(dp[i+j] != -1)
-                    dp[i] = Math.min(dp[i] , dp[i+j]+1);
-                
+            for(int j = 1 ; j <= jump ; j++){
+                if(j+i < nums.length && dp[i+j] != Integer.MAX_VALUE){
+                    dp[i] = Math.min(dp[i] , 1+dp[i+j]) ;
+                }
             }
         }
         return dp[0];
