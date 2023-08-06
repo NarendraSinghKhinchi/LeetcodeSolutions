4725f64a9dd3f705602d97cc221a2496ee4d4d1c
NarendraSinghKhinchi <110231704+NarendraSinghKhinchi@users.noreply.github.com>
2023-08-06 11:17:44 +0530

    Added solution - LeetHub

---
+++ String Permutations - GFG/string-permutations.java
@@ -0,0 +1,61 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+/*package whatever //do not write package name here */
+
+import java.io.*;
+import java.util.*;
+class Main {
+	public static void main (String[] args) {
+		Scanner sc=new Scanner(System.in);
+		
+		int T=sc.nextInt();
+		sc.nextLine();
+		while(T-->0)
+		{
+		    
+		    Solution ob=new Solution();
+		    
+		    String S=sc.nextLine();
+		    
+		    ArrayList<String> arr = ob.permutation(S);
+		    for(String s : arr){
+		        System.out.print(s+" ");
+		    }
+		    System.out.println();
+		}
+		
+	}
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+
+class Solution
+{
+    public ArrayList<String> permutation(String S)
+    {
+        //Your code here
+        ArrayList<String> list = new ArrayList<>();
+        permu(S , "" ,list);
+        Collections.sort(list);
+        return list ;
+    }
+    public void permu(String s , String cur , ArrayList<String> list){
+        if(s.length() == 0){
+            list.add(cur);
+            return ;
+        }
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            String left_substr = s.substring(0, i);
+            String right_substr = s.substring(i + 1);
+            String rest = left_substr + right_substr;
+            permu(rest, cur + ch , list);
+        }
+    }
+	   
+}
