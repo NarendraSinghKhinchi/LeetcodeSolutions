8b07a0cc8be1afe627bcf4610c6b63d05d018d01
NarendraSinghKhinchi <110231704+NarendraSinghKhinchi@users.noreply.github.com>
2023-08-26 00:21:02 +0530

    Added solution - LeetHub

--- Longest K unique characters substring - GFG/longest-k-unique-characters-substring.java
+++ Longest K unique characters substring - GFG/longest-k-unique-characters-substring.java
@@ -24,23 +24,21 @@ class GfG {
 class Solution {
     public int longestkSubstr(String s, int k) {
         // code here
-        if(k == 0)return 0 ;
-        HashMap<Character , Integer> hm = new HashMap<>() ;
-        int longest = -1 ;
-        int j = 0 ;
-        for(int i = 0 ; i < s.length() ; i++){
-            char ch = s.charAt(i);
-            hm.put(ch , hm.getOrDefault(ch ,0)+1);
-            
-            while(hm.size() > k && j < i ){
-                char ch2 = s.charAt(j++);
-                hm.put(ch2 , hm.get(ch2)-1);
-                if(hm.get(ch2) == 0)hm.remove(ch2);
-            }  
-            if(hm.size() == k){
-                longest = Math.max(longest , i-j+1);
+        HashMap<Character,Integer> hm = new HashMap<>();
+        int i = 0 ;
+        int len = -1 ;
+        for(int j = 0 ; j < s.length() ; j++){
+            char ch= s.charAt(j);
+            hm.put(ch,hm.getOrDefault(ch,0)+1);
+            while(hm.size() > k){
+                char ch2 = s.charAt(i);
+                hm.put(ch2,hm.get(ch2)-1);
+                if(hm.get(ch2)==0)hm.remove(ch2);
+                i++ ;
             }
+            if(hm.size() == k)
+                len = Math.max(len , j-i+1);
         }
-        return longest ;
+        return len ;
     }
 }
