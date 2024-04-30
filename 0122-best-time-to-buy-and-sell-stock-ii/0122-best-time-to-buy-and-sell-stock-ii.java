db1a9065315ce7b68d1278203823a22528f292c3
NarendraSinghKhinchi <110231704+NarendraSinghKhinchi@users.noreply.github.com>
2024-05-01 00:02:28 +0530

    Time: 1 ms (92.90%), Space: 45.8 MB (40.25%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
@@ -1,30 +1,18 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        int dp[][] = new int[prices.length+1][2];
-        // for(int d[] : dp)Arrays.fill(d , -1);
-        // int profit = recurs(0 , 0 , prices , dp);
-        for(int i = prices.length-1 ; i >= 0 ; i--){
-            for(int j = 0 ; j < 2 ; j++){
-                if(j == 0){
-                    dp[i][j] = Math.max(dp[i+1][0] , dp[i+1][1]-prices[i]) ;
-                }else{
-                    dp[i][j] = Math.max(dp[i+1][0]+prices[i] , dp[i+1][1]) ;
-                }
+        int maxprofit = 0 ;
+        int cost = prices[0];
+        int sell = prices[0] ;
+        for(int i = 1; i < prices.length; i++){
+            if(prices[i] < sell){
+                maxprofit += sell-cost ;
+                cost = prices[i] ;
+                sell = prices[i] ;
+            }else{
+                sell = Math.max(sell, prices[i]);
             }
         }
-        return dp[0][0] ;
+        maxprofit += sell-cost ;
+        return maxprofit ;
     }
-    // 0 means you can buy 1 means already bought
-//     public int recurs(int idx , int buy , int[]prices , int dp[][] ){
-//         if(idx == prices.length)return 0 ;
-//         if(dp[idx][buy] != -1)return dp[idx][buy] ;
-//         int profit = 0 ;
-//         if(buy == 0){
-//             profit = Math.max(recurs(idx+1 , 0 , prices , dp) , recurs(idx+1 , 1 , prices , dp)-prices[idx]);
-//         }else
-//             profit = Math.max(recurs(idx+1 , 0 , prices , dp) + prices[idx] , recurs(idx+1 , 1 , prices , dp));
-        
-//         dp[idx][buy] = profit ;
-//         return profit ;
-//     }
 }
