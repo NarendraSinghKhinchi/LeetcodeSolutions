7eb4e25a6c770b06b0687a8420710a237aa0f474
NarendraSinghKhinchi <110231704+NarendraSinghKhinchi@users.noreply.github.com>
2024-05-07 08:23:52 +0530

    Time: 4 ms (7.16%), Space: 55.1 MB (30.10%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.java
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.java
@@ -1,25 +1,24 @@
 class Solution {
     public int[] productExceptSelf(int[] nums) {
-        int pre[] = new int[nums.length];
-        int pos[] = new int[nums.length];
+        int pre[] = new int[nums.length] ;
+        int post[] = new int[nums.length] ;
+        
         pre[0] = nums[0] ;
-        for(int i = 1 ;i < nums.length ; i++){
-            pre[i] = pre[i-1]*nums[i] ;
+        post[nums.length-1] = nums[nums.length-1] ;
+        for(int i = 0 ; i < nums.length; i++){
+            if(i > 0) pre[i] = nums[i]*pre[i-1] ;
+            if(i < nums.length-1){
+                int j = nums.length-i-2 ;
+                post[j] = post[j+1]*nums[j] ;
+            }
         }
+        int ans[] = new int[nums.length] ;
         
-        pos[nums.length-1] = nums[nums.length-1] ;
-        for(int i = nums.length-2 ; i >= 0 ; i--){
-            pos[i] = pos[i+1]*nums[i] ;
+        for(int i = 0 ; i < nums.length ; i++){
+            if(i == 0)ans[i] = post[i+1] ;
+            else if (i == nums.length-1) ans[i] = pre[i-1] ;
+            else ans[i] = pre[i-1]*post[i+1] ;
         }
-        
-        int arr[] = new int[nums.length];
-        arr[0] = pos[1];
-        arr[nums.length-1] = pre[nums.length-2];
-        
-        for(int i = 1 ;i < nums.length-1 ; i++){
-            arr[i] = pre[i-1]*pos[i+1];
-        }
-        
-        return arr ;
+      return ans ;
     }
 }
